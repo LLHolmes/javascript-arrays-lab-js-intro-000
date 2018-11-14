85d89b89c6ba9660c76fc10ec4baf3f0c19fe16f
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-14 01:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name) {
   return (kittens.push(name));
 }
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  return (kittens.unshift(name));
+}
